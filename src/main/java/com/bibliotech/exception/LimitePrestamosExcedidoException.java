9589f3c8ccab29c48b73d888215517c2322c56cb
v-perassiferrara <v.perassi@alumno.um.edu.ar>
2026-04-20 08:35:50 -0300

    feat(exception): add custom exceptions for resources, members and loans

---
+++ src/main/java/com/bibliotech/exception/LimitePrestamosExcedidoException.java
@@ -0,0 +1,7 @@
+package com.bibliotech.exception;
+
+public class LimitePrestamosExcedidoException extends PrestamoException {
+    public LimitePrestamosExcedidoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/bibliotech/exception/PrestamoException.java
@@ -0,0 +1,7 @@
+package com.bibliotech.exception;
+
+public class PrestamoException extends BibliotecaException {
+    public PrestamoException(String message) {
+        super(message);
+    }
+}
