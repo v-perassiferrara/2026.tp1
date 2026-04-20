89dee401759f26c623be1499d0efe7d6ac9fa3b4
v-perassiferrara <v.perassi@alumno.um.edu.ar>
2026-04-20 07:55:08 -0300

    feat(model): add manual return validation for resources

--- src/main/java/com/bibliotech/model/LibroFisico.java
+++ src/main/java/com/bibliotech/model/LibroFisico.java
@@ -1,5 +1,8 @@
 package com.bibliotech.model;
 
 public record LibroFisico(String isbn, String titulo, String autor, int anio, int categoria, int paginas) implements Recurso {
-
+    @Override
+    public Boolean requiereDevolucionManual() {
+        return Boolean.TRUE;
+    }
 }
