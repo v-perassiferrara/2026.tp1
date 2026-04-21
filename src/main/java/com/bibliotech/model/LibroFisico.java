cc2b2747f59ba21c5fac5bfb2f8011fb1e199b4d
v-perassiferrara <v.perassi@alumno.um.edu.ar>
2026-04-21 09:27:05 -0300

    refactor(model): change category type from int to string

--- src/main/java/com/bibliotech/model/LibroFisico.java
+++ src/main/java/com/bibliotech/model/LibroFisico.java
@@ -1,12 +1,13 @@
 package com.bibliotech.model;
 
-public record LibroFisico(String isbn, String titulo, String autor, int anio, int categoria, int paginas) implements Recurso {
+public record LibroFisico(String isbn, String titulo, String autor, int anio, String categoria, int paginas) implements Recurso {
 
     // Para validaciones de formato, no de negocio
     public LibroFisico {
         if (isbn == null || isbn.isBlank()) throw new IllegalArgumentException("ISBN es obligatorio");
         if (titulo == null || titulo.isBlank()) throw new IllegalArgumentException("Título es obligatorio");
         if (autor == null || autor.isBlank()) throw new IllegalArgumentException("Autor es obligatorio");
+        if (categoria == null || categoria.isBlank()) throw new IllegalArgumentException("Categoría es obligatoria");
         if (anio <= 0) throw new IllegalArgumentException("Año debe ser positivo");
         if (paginas <= 0) throw new IllegalArgumentException("Número de páginas debe ser positivo");
     }
